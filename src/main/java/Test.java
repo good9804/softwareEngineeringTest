7348c53b4813ebb48e39842c4507d64ee9e16e79
ggoniggoni <40421199+ggoniggoni@users.noreply.github.com>
2023-03-26 14:17:58 +0900

    Update Test.java
    gunhee->gun

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is gunhee");
+        System.out.println("My name is gun");
         System.out.println("AAA");
     }
 }
