0ca4445db4d7a57f29bbea017c5ef989890b25c3
ggoniggoni <40421199+ggoniggoni@users.noreply.github.com>
2023-03-26 14:18:10 +0900

    Update Test.java
    AAA->BBB

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("My name is gun");
-        System.out.println("AAA");
+        System.out.println("BBB");
     }
 }
